3ea5a4aa039bd12a740ef1fac0f7af5a3eda9264
Kamisama <953181215@qq.com>
2019-07-11 22:25:49 +0800

    finish practice08

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,37 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce(){
+        if (klass==null){
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }else {
+            return super.introduce() + " I am a Teacher. I teach "+klass.getDisplayName()+".";
+        }
+    }
+
+    public String introduceWith(Student student){
+        if (student.getKlass().equals(this.klass)){
+            return super.introduce() + " I am a Teacher. I teach "+student.getName()+".";
+        } else {
+            return super.introduce() + " I am a Teacher. I don't teach "+student.getName()+".";
+        }
+
+    }
+
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
